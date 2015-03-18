cb5b46f1e72c0e65284024a4d04c923209df1b28
Xianda Wang <wxianda@gmail.com>
2015-03-18 18:45:23 +0800

    Fix MinGW build on trusty

--- port/port_win.cc
+++ port/port_win.cc
@@ -6,7 +6,7 @@
 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 //  * Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 //  * Redistributions in binary form must reproduce the above copyright
@@ -15,7 +15,7 @@
 //  * Neither the name of the University of California, Berkeley nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -62,9 +62,9 @@ void Mutex::AssertHeld() {
 }
 
 CondVar::CondVar(Mutex* mu) :
-    waiting_(0), 
-    mu_(mu), 
-    sema_(::CreateSemaphore(NULL, 0, 0x7fffffff, NULL)), 
+    waiting_(0),
+    mu_(mu),
+    sema_(::CreateSemaphore(NULL, 0, 0x7fffffff, NULL)),
     event_(::CreateEvent(NULL, FALSE, FALSE, NULL)),
     broadcasted_(false){
   assert(mu_);
@@ -134,7 +134,7 @@ AtomicPointer::AtomicPointer(void* v) {
 }
 
 void* AtomicPointer::Acquire_Load() const {
-  void * p = nullptr;
+  void * p = NULL;
   InterlockedExchangePointer(&p, rep_);
   return p;
 }
--- port/port_win.h
+++ port/port_win.h
@@ -6,7 +6,7 @@
 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 //  * Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 //  * Redistributions in binary form must reproduce the above copyright
@@ -15,7 +15,7 @@
 //  * Neither the name of the University of California, Berkeley nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -90,14 +90,14 @@ class CondVar {
   void SignalAll();
  private:
   Mutex* mu_;
-  
+
   Mutex wait_mtx_;
   long waiting_;
-  
+
   void * sema_;
   void * event_;
 
-  bool broadcasted_;  
+  bool broadcasted_;
 };
 
 // Storage for a lock-free pointer
@@ -105,8 +105,8 @@ class AtomicPointer {
  private:
   void * rep_;
  public:
-  AtomicPointer() : rep_(nullptr) { }
-  explicit AtomicPointer(void* v); 
+  AtomicPointer() : rep_(NULL) { }
+  explicit AtomicPointer(void* v);
   void* Acquire_Load() const;
 
   void Release_Store(void* v);
